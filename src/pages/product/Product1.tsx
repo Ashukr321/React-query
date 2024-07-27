015a99ac4037e27de6aa92ada0c2b1131231bcc5
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-07-27 13:24:39 +0530

    fetch the data from the api and list all the product using fetch api

--- src/pages/product/Product1.tsx
+++ src/pages/product/Product1.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Loader from "../../compoentns/loader/Loader";
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  images: string[];
+}
 
 const Product1: React.FC = () => {
-  return <div>
-    <h1>welcome to product page </h1>
-  </div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        setProducts(data.products);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl text-center mb-4 font-bold tracking-tight text-gray-900">
+          LIST OF PRODUCTS
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative cursor-pointer">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  alt={product.title}
+                  src={product.images[0]}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+
+              <div className="mt-4 flex justify-between">
+                <p>{product.description}</p>
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.title}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.rating}</p>
+                </div>
+
+                <p className="text-sm font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product1;
